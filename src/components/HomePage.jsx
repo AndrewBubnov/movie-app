2ac66c8b3f73c00e3170a5e97be6722f072b9bd9
AndrewBubnov <andrew.bubnov75@gmail.com>
2020-09-23 14:46:38 +0300

    search and life search switch by long tap on textfield implemented

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -8,21 +8,40 @@ import {debounce} from "lodash";
 
 const HomePage = ({store: {setSearchString}}) => {
     const [search, setSearch] = useState('');
+    const [mouseDownTime, setMouseDownTime] = useState(null);
+    const [isLifeSearchActive, setIsLifeSearchActive] = useState(false);
     const { push } = useHistory();
 
     const debouncedPush = useCallback(debounce(push, 500),[]);
+    const debouncedSearch = useCallback(debounce(setSearchString, 500),[]);
 
     const handleChange = (e) => {
         const {target: { value }} = e;
         setSearch(value);
-        debouncedPush(`/list?s=${value}`)
+        isLifeSearchActive ?
+            debouncedSearch(value)
+            : debouncedPush(`/list?s=${value}`);
     }
+
+    const handleMouseDown = () => {
+        setMouseDownTime(Date.now());
+    }
+
+    const handleMouseUp = () => {
+        if (Date.now() - mouseDownTime >= 1000) {
+            setIsLifeSearchActive(prevState => !prevState);
+        }
+        setMouseDownTime(null);
+    }
+
     return (
         <div className="App">
             <header className="App-header">
                 <TextField
                     value={search}
                     onChange={handleChange}
+                    onMouseDown={handleMouseDown}
+                    onMouseUp={handleMouseUp}
                 />
             </header>
         </div>
